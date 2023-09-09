c43a25edb6b78322f49746320eb939e2280fc1d0
Maziyar Gerami <maziyar.grami@gmail.com>
2023-09-10 00:00:00 +0330

    getCategoriesAll.php modified

---
+++ Chili/Pages/getCategoriesAll.php
@@ -0,0 +1,82 @@
+<?php
+ error_reporting(E_ALL); ini_set('display_errors', 1);
+ header('Content-Type: application/json; charset=utf-8');
+ 
+/*
+ * Following code will get single product details
+ * A product is identified by product id (pid)
+ */
+ 
+// array for JSON response
+$response = array();
+ 
+// include db connect class
+require_once __DIR__ . '/db_connect.php';
+ 
+// connecting to db
+$db = new DB_CONNECT();
+
+ 
+// check for post data
+if (1) {
+	
+    // get a product from products table
+    $result = mysqli_query($db->connect(),"SELECT * FROM categories");
+	
+	
+
+    if (!empty($result)) {
+        // check for empty result
+        if (mysqli_num_rows($result) > 0) {
+           
+			
+			while ($resultC = $result->fetch_assoc()) {
+				$cat= array();
+				$cat["Id_Cat"] = $resultC["Id_Cat"];
+				$cat["Name"] = $resultC["Name"];
+				$cat["Parent"] = ($resultC["Parent"]);
+				$ID = $resultC["Id_Cat"];
+				
+				$resultLeaf = mysqli_query($db->connect(),"SELECT * FROM categories WHERE Parent = $ID");
+				
+				if (mysqli_num_rows($resultLeaf) > 0) {
+					
+					$cat["isLeaf"] = false;
+					
+				}else{
+					
+					$cat["isLeaf"] = true;
+					
+				}
+				
+				
+				array_push($response, $cat);
+			}
+ 
+            // echoing JSON response
+            echo json_encode($response,JSON_UNESCAPED_UNICODE);
+        } else {
+            // no product found
+            $response["success"] = 02;
+            $response["message"] = "No product found";
+ 
+            // echo no users JSON
+            echo json_encode($response);
+        }
+    } else {
+        // no product found
+        $response["success"] = 03;
+        $response["message"] = "No product found";
+ 
+        // echo no users JSON
+        echo json_encode($response);
+    }
+} else {
+    // required field is missing
+    $response["success"] = 0;
+    $response["message"] = "Required field(s) is missing";
+ 
+    // echoing JSON response
+    echo json_encode($response);
+}
+?>
