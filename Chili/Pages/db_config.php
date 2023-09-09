ef0d8f5e8544db4d02a49c930a7e82d9f836586b
Maziyar Gerami <maziyar.grami@gmail.com>
2023-09-10 00:00:00 +0330

    db_config.php modified

---
+++ Chili/Pages/db_config.php
@@ -0,0 +1,11 @@
+<?php
+ 
+/*
+ * All database connection variables
+ */
+ 
+define('DB_USER', "id1572888_root"); // db user
+define('DB_PASSWORD', "09180918"); // db password (mention your db password here)
+define('DB_DATABASE', "id1572888_foodapp"); // database name
+define('DB_SERVER', "localhost"); // db server
+?>
